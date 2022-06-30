d1763b4deb7d067fa65290beb5510f5e3602af0b
shutorm02 <shutorm2106@gmail.com>
2022-06-30 11:47:30 +0900

    Add: vue3 の設定を追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } =require('vue-loader')
+
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
+
+environment.loaders.prepend('vue', {
+    test: /\.vue$\,
+    user: [{
+        loader: 'vue-loader'
+    }]
+]}
 
 module.exports = environment
