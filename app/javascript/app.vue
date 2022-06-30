7ad7b18a36c83886ad4db1386192f96ff383c396
shutorm02 <shutorm2106@gmail.com>
2022-07-01 08:31:10 +0900

    Add: vue コンポーネントを作成

---
+++ app/javascript/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>
+    {{ message }}
+  </p>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const message = ref('Hello World')
+    return { message }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: left;
+}
+</style>
