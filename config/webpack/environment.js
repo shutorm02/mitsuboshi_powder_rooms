0b899cfbfece82e2012f19940b488762b1cf4c7d
shutorm02 <shutorm2106@gmail.com>
2022-07-01 09:42:05 +0900

    Add: Vue のオプションAPI の設定を追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,18 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
+const { DefinePlugin } = require('webpack')
 
 environment.plugins.prepend(
     'VueLoaderPlugin',
     new VueLoaderPlugin()
+)
+
+environment.plugins.prepend(
+  'Define',
+  new DefinePlugin({
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+  })
 )
 
 environment.loaders.prepend('vue', {
