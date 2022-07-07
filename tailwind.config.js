8df8fa9d5ec6c931745ea714d33fa01c2b61c4d5
shutorm02 <shutorm2106@gmail.com>
2022-07-07 17:47:57 +0900

    Add: tailwind にカスタムカラーを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'thema': '#FFF5E9',
+        'fitting-pink': '#EABEBF',
+        'powder-pink': '#D6A3DC',
+        'baby-blue': '#A5DEE5',
+        'twitter-blue': '#1DA1F2',
+        'mitsuboshi-blue': '#75CCE8',
+        'mitsuboshi-yellow': '#F7DB70',
+        'mitsuboshi-gray': '#E5E5E5',
+      }
+    },
   },
   variants: {
     extend: {},
