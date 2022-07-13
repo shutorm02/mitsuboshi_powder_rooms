34fe6fcf42e27573c5c18f50ec835fad34f19a2f
shutorm02 <shutorm2106@gmail.com>
2022-07-13 19:57:18 +0900

    Add: config/routes に ログイン関連のアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'login', to: 'user_session#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create destroy]
 end
