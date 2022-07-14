b3803f96b5fd5d18ddcba62f48bfa4e8860481e2
shutorm02 <shutorm2106@gmail.com>
2022-07-14 15:41:50 +0900

    Add: user_sessions コントローラーに new と create メソッドを作成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,12 @@
 class UserSessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path
+    else
+      render :new
+    end
+  end
 end
