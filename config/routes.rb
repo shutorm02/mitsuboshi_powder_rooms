22856f190ec53ab9f501c2bf1ee71f3cfd5aeac7
shutorm02 <shutorm2106@gmail.com>
2022-07-14 16:01:01 +0900

    Fix: ルーティングエラーを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  get 'login', to: 'user_session#new'
+  get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
