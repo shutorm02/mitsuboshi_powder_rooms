fc4432d876eb3fd4081a1537c94331a13e294834
shutorm02 <shutorm2106@gmail.com>
2022-07-14 18:27:20 +0900

    Fix: rubocop にてコードを自動修正

--- db/migrate/20220713102854_sorcery_core.rb
+++ db/migrate/20220713102854_sorcery_core.rb
@@ -1,14 +1,14 @@
 class SorceryCore < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email,            null: false, index: { unique: true }
+      t.string :email, null: false, index: { unique: true }
       t.string :name, null: false
       t.string :crypted_password
       t.string :salt
       t.string :avator
       t.integer :role, default: 0
 
-      t.timestamps                null: false
+      t.timestamps null: false
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name                  {"text"}
-    email                 {"test@example.com"}
-    password              {"password"}
-    password_confirmation {"password"}
+    name                  { 'text' }
+    email                 { 'test@example.com' }
+    password              { 'password' }
+    password_confirmation { 'password' }
   end
 end
