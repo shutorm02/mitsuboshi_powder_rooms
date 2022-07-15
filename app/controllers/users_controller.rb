7323610fb2ede4b9bfe79f377d56680f671f9d24
shutorm02 <shutorm2106@gmail.com>
2022-07-15 20:27:46 +0900

    Add: ユーザー作成時の flash message を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to login_path, success: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
