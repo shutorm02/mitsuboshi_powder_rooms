03f48d9ec80cf44292c788a7a96855da9efc21cb
shutorm02 <shutorm2106@gmail.com>
2022-07-17 09:00:16 +0900

    Add: RSpec に Bullet の設定を追加

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,6 @@
 require "active_support/core_ext/integer/time"
+require "active_record"
+require "bullet"
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
