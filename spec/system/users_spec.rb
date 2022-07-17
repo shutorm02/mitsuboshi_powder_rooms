30cb1965e8cc6adff0d567dc695eee9eec194e4b
shutorm02 <shutorm2106@gmail.com>
2022-07-17 14:21:49 +0900

    Add: ユーザーの新規登録処理をテスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe 'ユーザー新規登録' do
+    context 'フォームの入力値が正常' do
+      it 'ユーザーの新規登録が成功する' do
+        visit new_user_path
+        fill_in 'アカウント名', with: 'user_test'
+        fill_in 'メールアドレス', with: 'user_test@example.com'
+        fill_in 'パスワード', with: 'password'
+        fill_in 'パスワード確認', with: 'password'
+        click_button '登録'
+        expect(page).to have_content 'ユーザー登録が完了しました'
+        expect(current_path).to eq login_path
+      end
+    end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    context 'アカウント名が未入力' do
+      it 'ユーザーの新規登録が失敗する' do
+        visit new_user_path
+        fill_in 'アカウント名', with: ''
+        fill_in 'メールアドレス', with: 'user_test@example.com'
+        fill_in 'パスワード', with: 'password'
+        fill_in 'パスワード確認', with: 'password'
+        click_button '登録'
+        expect(page).to have_content 'ユーザー登録に失敗しました'
+        expect(page).to have_content 'アカウント名を入力してください'
+        expect(current_path).to eq users_path
+      end
+    end
+
+    context 'メールアドレスが未入力' do
+      it 'ユーザーの新規登録が失敗する' do
+        visit new_user_path
+        fill_in 'アカウント名', with: 'user_test'
+        fill_in 'メールアドレス', with: ''
+        fill_in 'パスワード', with: 'password'
+        fill_in 'パスワード確認', with: 'password'
+        click_button '登録'
+        expect(page).to have_content 'ユーザー登録に失敗しました'
+        expect(page).to have_content 'メールアドレスを入力してください'
+        expect(current_path).to eq users_path
+      end
+    end
+
+    context '登録済みのメールアドレスを使用' do
+      it 'ユーザーの新規登録が失敗する' do
+        exsited_user = create(:user)
+        visit new_user_path
+        fill_in 'アカウント名', with: 'user_test'
+        fill_in 'メールアドレス', with: exsited_user.email
+        fill_in 'パスワード', with: 'password' 
+        fill_in 'パスワード確認', with: 'password'
+        click_button '登録'
+        expect(page).to have_content 'ユーザー登録に失敗しました'
+        expect(page).to have_content 'メールアドレスはすでに存在します'
+        expect(current_path).to eq users_path
+      end
+    end
+  end
 end
