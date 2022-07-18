ebc640c705c32fa9d73c3b328ef8a80b3a26f0f0
shutorm02 <shutorm2106@gmail.com>
2022-07-18 20:57:20 +0900

    Update: Spot のカラムにバリデーションを追加

--- db/migrate/20220718092330_create_spots.rb
+++ db/migrate/20220718092330_create_spots.rb
@@ -1,10 +1,10 @@
 class CreateSpots < ActiveRecord::Migration[6.1]
   def change
     create_table :spots do |t|
-      t.string :name
-      t.string :address
-      t.float :latitude
-      t.float :longitude
+      t.string :name, null: false
+      t.string :address, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
