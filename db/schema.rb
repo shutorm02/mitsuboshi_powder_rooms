16cca2ce5fd94a66d4dff36c8aabb09e90b29f07
shutorm02 <shutorm2106@gmail.com>
2022-07-19 12:03:44 +0900

    Add: bundle exec rails db:migrate を実行、Spotテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_13_102854) do
+ActiveRecord::Schema.define(version: 2022_07_18_092330) do
+
+  create_table "spots", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_spots_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -24,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_07_13_102854) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "spots", "users"
 end
