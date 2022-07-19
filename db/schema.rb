326e3c2e88edc131c458b1d57bcecf04960333c5
shutorm02 <shutorm2106@gmail.com>
2022-07-19 15:49:25 +0900

    Update:  bundle exec rails db:migrate を実行、Not null 制約を追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_054629) do
+ActiveRecord::Schema.define(version: 2022_07_19_064243) do
 
   create_table "equipment", force: :cascade do |t|
     t.integer "spot_id", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2022_07_19_054629) do
   end
 
   create_table "equipment_details", force: :cascade do |t|
-    t.string "content"
+    t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
