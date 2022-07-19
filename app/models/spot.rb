7c6d468a6850aab46a3ec1eb71d1d48a74ffdfc8
shutorm02 <shutorm2106@gmail.com>
2022-07-19 12:01:04 +0900

    Add: Spot モデルにバリデーションを追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,8 @@
 class Spot < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 end
