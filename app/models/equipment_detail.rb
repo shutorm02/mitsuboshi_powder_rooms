6c5d69f148d1c0a92a4b89ee28d8d7195513bb0e
shutorm02 <shutorm2106@gmail.com>
2022-07-19 20:33:46 +0900

    Update: equipment_detail と target_person にそれぞれの関係性を追加

--- app/models/equipment_detail.rb
+++ app/models/equipment_detail.rb
@@ -1,6 +1,7 @@
 class EquipmentDetail < ApplicationRecord
   has_many :equipments, dependent: :destroy
   has_many :spots, through: :equipments
+  belongs_to :target_person
 
   validates :content, presence: true
 end
