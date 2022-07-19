d2c9258ab38b1ec20251f85f306c2fe0ee0fa426
shutorm02 <shutorm2106@gmail.com>
2022-07-19 15:24:04 +0900

    Add: 各モデルに設定を追加

--- app/models/equipment_detail.rb
+++ app/models/equipment_detail.rb
@@ -1,2 +1,4 @@
 class EquipmentDetail < ApplicationRecord
+  has_many :equipments, dependent: :destroy
+  has_many :spots, through: :equipments
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,8 @@
 class Spot < ApplicationRecord
   belongs_to :user
+  has_many :equipments, dependent: :destroy
+  has_many :equipment_details, dependent: :destroy
+  has_many :equipment_equipment_details, through: :equipments, source: :equipment_details
 
   validates :name, presence: true
   validates :address, presence: true
