c90d49012ff6b0392488c1586cbf27318f472172
shutorm02 <shutorm2106@gmail.com>
2022-07-20 16:17:53 +0900

    Update: seed-fu の記述をシンタックスシュガーに変更

--- db/fixtures/01_target_people.rb
+++ db/fixtures/01_target_people.rb
@@ -1,14 +1,5 @@
-targetperson.seed_once do |s|
-  s.id = 1
-  s.target = 'きれい好きな人'
-end
-
-TargetPerson.seed do |s|
-  s.id = 2
-  s.target = 'おしゃれが好きな女性'
-end
-
-TargetPerson.seed do |s|
-  s.id = 3
-  s.target = '小さなお子さん連れの女性'
-end
+TargetPerson.seed_once(:id,
+  { id: 1, target: 'きれい好きな人' },
+  { id: 2, target: 'おしゃれが好きな女性' },
+  { id: 3, target: '小さなお子さん連れの女性' },
+)
