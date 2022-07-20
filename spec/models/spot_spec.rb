882b317c97dfe36f059c59497473628172ac0439
shutorm02 <shutorm2106@gmail.com>
2022-07-20 21:08:02 +0900

    Add: spot モデルのテストコードを作成

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'すべてのフィールドが有効な場合' do
+    it '有効であること' do
+      spot = build(:spot)
+      expect(spot).to be_valid
+    end
+  end
+
+  context 'スポット名が存在しない場合' do
+    it '無効であること' do
+      spot = build(:spot, name: nil)
+      expect(spot).to be_invalid
+      expect(spot.errors[:name]).to include('を入力してください')
+    end
+  end
+  
+  context '住所が存在しない場合' do
+    it '無効であること' do
+      spot = build(:spot, address: nil)
+      expect(spot).to be_invalid
+      expect(spot.errors[:address]).to include('を入力してください')
+    end
+  end
+  
+  context '緯度が存在しない場合' do
+    it '無効であること' do
+      spot = build(:spot, latitude: nil)
+      expect(spot).to be_invalid
+      expect(spot.errors[:latitude]).to include('を入力してください')
+    end
+  end
+
+  context '経度が存在しない場合' do
+    it '無効であること' do
+      spot = build(:spot, longitude: nil)
+      expect(spot).to be_invalid
+      expect(spot.errors[:longitude]).to include('を入力してください')
+    end
+  end
 end
