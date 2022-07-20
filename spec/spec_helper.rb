7abc40aa07f3cfb319f12128e036c0ceb22feca0
shutorm02 <shutorm2106@gmail.com>
2022-07-20 16:51:59 +0900

    Add: RSpec 実行時、Seed Fu が実行されるように処理を追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,4 +91,7 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+    config.before(:suite) do
+      SeedFu.seed
+    end
 end
