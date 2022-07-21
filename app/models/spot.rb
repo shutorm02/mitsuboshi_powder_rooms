c14c260df311afb7c2b743891fb52937750ed990
shutorm02 <shutorm2106@gmail.com>
2022-07-21 18:50:30 +0900

    Fix: spot のアソシエーションを変更

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :user
   has_many :equipments, dependent: :destroy
-  has_many :equipment_details, dependent: :destroy
-  has_many :equipment_equipment_details, through: :equipments, source: :equipment_details
+  has_many :equipment_details, dependent: :destroy, through: :equipments
 
   validates :name, uniqueness: true, presence: true
   validates :address, uniqueness: true, presence: true
