9cefec8844a282ee86b7fdb4490321fd8e083dfc
shutorm02 <shutorm2106@gmail.com>
2022-07-21 23:00:32 +0900

    Add: 設備詳細のタグの色を自動で変更させる

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -1,2 +1,13 @@
 module SpotsHelper
+  def tag_color(equipment_detail)
+    if equipment_detail.target_person_id == 1
+      return 'powder-pink'
+    elsif equipment_detail.target_person_id == 2
+      return 'powder-pink'
+    elsif equipment_detail.target_person_id == 3
+      return 'baby-blue'
+    else equipment_detail.target_person_id == 4 
+      return 'fitting-pink'
+    end
+  end
 end
