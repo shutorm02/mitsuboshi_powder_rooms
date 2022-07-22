688a321d59a52bb3bd26e1bbc0d9acd0c60b3c7c
shutorm02 <shutorm2106@gmail.com>
2022-07-22 10:02:26 +0900

    Update: スポット情報一覧のテストコードを修正

--- spec/system/spots_spec.rb
+++ spec/system/spots_spec.rb
@@ -2,14 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "Spots", type: :system do
   let(:user) { create(:user) }
-  let(:spot) { ccreate(:spot, user: user) }
+  let(:spot) { create(:spot, user: user) }
 
   describe 'スポット情報のCRUD' do
     describe 'スポット情報の一覧' do
-      it 'フッターのリンクからスポット情報一覧へ遷移できること' do
-        click_link '検索'
-        click_on('リスト表示')
+      it 'ナビゲーションバーのリンクからスポット情報一覧へ遷移できること' do
+        visit root_path
+        click_on('検索')
         expect(current_path).to eq(spots_path)
+      end
+
+      context 'スポット情報が一件もない場合' do
+        it '「スポット情報がありません」と表示されること' do
+          visit spots_path
+          expect(page).to have_content('登録されているスポットがありません')
+        end
+      end
+
+      context 'スポット情報がある場合' do
+        it 'スポット情報の一覧が表示されること' do
+          spot
+          visit spots_path
+          expect(page).to have_content(spot.name)
+        end
       end
     end
   end
