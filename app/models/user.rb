b7f965eb8a3fa03730356b043643038c12e8e51e
shutorm02 <shutorm2106@gmail.com>
2022-07-23 21:05:40 +0900

    Update: User モデルに Spot のアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  
+  has_many :spots
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
