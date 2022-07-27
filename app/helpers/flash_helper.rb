b16ecdbd5f06ab4a7832867a55d84787e657914e
shutorm02 <shutorm2106@gmail.com>
2022-07-27 18:32:04 +0900

    Update: rubocop により、コードを手修正

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,11 +1,12 @@
 module FlashHelper
+  FLASH = {
+    'success' => 'text-green-700 bg-green-100 rounded-lg p-4 mb-4',
+    'info' => 'text-blue-700 bg-blue-100 rounded-lg p-4 mb-4',
+    'warning' => 'text-yellow-700 bg-yellow-100 rounded-lg p-4 mb-4',
+    'danger' => 'text-red-700 bg-red-100 rounded-lg p-4 mb-4',
+  }.freeze
 
-  def flash_class(message_type)
-    case message_type
-      when 'success' then 'text-green-700 bg-green-100 rounded-lg p-4 mb-4'
-      when 'info' then 'text-blue-700 bg-blue-100 rounded-lg p-4 mb-4'
-      when 'warning' then 'text-yellow-700 bg-yellow-100 rounded-lg p-4 mb-4'
-      when 'danger' then 'text-red-700 bg-red-100 rounded-lg p-4 mb-4'
-    end
+  def flash_class(type)
+    FLASH[type]
   end
 end
