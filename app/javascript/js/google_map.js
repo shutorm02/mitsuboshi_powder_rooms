33f7e07b12533651e8c46e60cd7745955eca2891
shutorm02 <shutorm2106@gmail.com>
2022-08-04 22:05:08 +0900

    Update: google map を initMap で読み込み

--- app/javascript/js/google_map.js
+++ app/javascript/js/google_map.js
@@ -2,25 +2,21 @@ require('dotenv').config()
 
 const googleMapsApi = process.env.GOOGLE_MAPS_API_KEY
 const script = document.createElement('script')
-script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApi}`
+script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApi}&callback=initMap`
 script.async = true
 document.head.appendChild(script)
 
-let map
-let timer = setInterval(() => {
-  if (window.google) {
-    clearInterval(timer)
-    const defaultsLocation = new window.google.maps.LatLng(
-      35.6803997,
-      139.7690174
-    )
-    map = new window.google.maps.Map(document.getElementById('map'), {
-      center: defaultsLocation,
-      zoom: 14,
-    })
-    new window.google.maps.Marker({
-      position: defaultsLocation,
-      map,
-    })
-  }
-}, 500)
+window.initMap = () => {
+  const defaultsLocation = new window.google.maps.LatLng(
+    35.6803997,
+    139.7690174
+  )
+  const map = new window.google.maps.Map(document.getElementById('map'), {
+    center: defaultsLocation,
+    zoom: 14,
+  })
+  new window.google.maps.Marker({
+    position: defaultsLocation,
+    map,
+  })
+}
