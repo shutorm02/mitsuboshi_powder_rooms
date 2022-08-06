97257a65242c3292465d16884a9dc9811f1fa901
shutorm02 <shutorm2106@gmail.com>
2022-08-06 22:29:48 +0900

    Add: 登録されたスポットをすべて表示

--- app/javascript/js/google_map.js
+++ app/javascript/js/google_map.js
@@ -8,15 +8,41 @@ document.head.appendChild(script)
 
 import iconCurrentLocation from '../images/icons/current_location.svg'
 
+let map
+const marker = []
+const infoWindow = []
+let spotLatLng
+const spotData = gon.spots
+
 window.initMap = () => {
   const defaultsLocation = new window.google.maps.LatLng(
     35.6803997,
     139.7690174
   )
-  const map = new window.google.maps.Map(document.getElementById('spotsMap'), {
+  map = new window.google.maps.Map(document.getElementById('spotsMap'), {
     center: defaultsLocation,
     zoom: 14,
   })
+
+  for (let i = 0; i < spotData.length; i++) {
+    let id = spotData[i]['id']
+
+    spotLatLng = new window.google.maps.LatLng({
+      lat: spotData[i]['latitude'],
+      lng: spotData[i]['longitude'],
+    })
+
+    marker[i] = new window.google.maps.Marker({
+      position: spotLatLng,
+      map: map,
+    })
+
+    infoWindow[i] = new window.google.maps.InfoWindow({
+      content: `<a href='/spots/${id}'>${spotData[i]['name']}</a>`,
+    })
+
+    markerEvent(i)
+  }
 
   new window.google.maps.Marker({
     position: defaultsLocation,
@@ -26,5 +52,11 @@ window.initMap = () => {
       scaledSize: new window.google.maps.Size(30, 46),
     },
     animation: window.google.maps.Animation.BOUNCE,
+  })
+}
+
+function markerEvent(i) {
+  marker[i].addListener('click', function () {
+    infoWindow[i].open(map, marker[i])
   })
 }
