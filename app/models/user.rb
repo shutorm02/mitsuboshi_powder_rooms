bcf066fbec2b4e6ec1f831a7319e720c2789b36f
shutorm02 <shutorm2106@gmail.com>
2022-08-12 15:19:40 +0900

    Add: feedback とのアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :spots
   has_many :likes, dependent: :destroy
   has_many :like_spots, through: :likes, source: :spot
+  has_many :feedbacks, dependent: :destroy
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
