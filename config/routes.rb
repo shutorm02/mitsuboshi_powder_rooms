918cedb7933440a83d649ad83b75976094460752
shutorm02 <shutorm2106@gmail.com>
2022-08-12 15:22:25 +0900

    Add: root に feedback#create、feedback#destroy を追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create destroy]
   resources :spots do
+    resources :feedbacks, only: %i[create destroy], shallow: true 
     collection do
       get :likes
     end
