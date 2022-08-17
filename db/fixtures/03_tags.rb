5c128ebae4fba78df001f392fd8b9e83b9a00597
shutorm02 <shutorm2106@gmail.com>
2022-08-17 09:10:37 +0900

    Add: Tag テーブルの初期データを設定

---
+++ db/fixtures/03_tags.rb
@@ -0,0 +1,12 @@
+Tag.seed_once(:id,
+  { id: 1, content: 'ホテルみたい！', target_person_id: 1 },
+  { id: 2, content: 'リラックスできる', target_person_id: 1 },
+  { id: 3, content: '気づかいを感じる', target_person_id: 1 },
+  { id: 4, content: 'いつもキレイでうれしい', target_person_id: 1 },
+  { id: 5, content: 'マキシ丈の服でも安心！', target_person_id: 2 },
+  { id: 6, content: '子供連れでも安心', target_person_id: 3 },
+  { id: 7, content: 'ベビーカー可がうれしい！', target_person_id: 3 },
+  { id: 8, content: 'お子さん連れの人におすすめしたい', target_person_id: 3 },
+  { id: 9, content: '着替える場所があって助かる！', target_person_id: 4 },
+)
+
