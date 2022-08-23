5e71fd25829ebd8d2d172ae10fc6d101b6fc91ad
shutorm02 <shutorm2106@gmail.com>
2022-08-23 18:34:16 +0900

    Update: feedback#new,create の変数を修正

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,23 +1,22 @@
 class FeedbacksController < ApplicationController
   skip_before_action :require_login, only: %i[index]
+  before_action :set_spot, only: %i[index new]
 
   def index
-    @spot = Spot.find(params[:spot_id])
     @feedbacks = @spot.feedbacks.includes(:user, :feedback_tags, :tags).order(created_at: :desc)
   end
 
   def new
-    @spot = Spot.find(params[:spot_id])
     @feedback_form = FeedbackForm.new
   end
 
   def create
-    feedback_form = FeedbackForm.new(feedback_params)
-    if feedback_form.save
-      redirect_to spot_path(feedback_form.spot_id),
+    @feedback_form = FeedbackForm.new(feedback_params)
+    if @feedback_form.save
+      redirect_to spot_path(@feedback_form.spot_id),
                   success: t('defaults.message.created', item: Feedback.model_name.human)
     else
-      redirect_to spot_path(feedback_form.spot_id),
+      redirect_to spot_path(@feedback_form.spot_id),
                   danger: t('defaults.message.not_created', item: Feedback.model_name.human)
     end
   end
@@ -32,5 +31,9 @@ class FeedbacksController < ApplicationController
       :feedback_comment,
       { tag_ids: [] },
     ).merge(user_id: current_user.id, spot_id: params[:spot_id])
+  end
+
+  def set_spot
+    @spot = Spot.find(params[:spot_id])
   end
 end
