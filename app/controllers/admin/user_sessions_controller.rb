1fbf1331f141cf225035ab9e56884651829c497f
shutorm02 <shutorm2106@gmail.com>
2022-09-03 16:54:51 +0900

    Add: user_session コントローラーに create、destroy を追加

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -4,4 +4,19 @@ class Admin::UserSessionsController < Admin::BaseController
   layout 'layouts/admin_login'
 
   def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_to admin_root_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
+    end
+  end
+
+  def logout
+    logout
+    redirect_to admin_login_path, success: t('.success')
+  end
 end
