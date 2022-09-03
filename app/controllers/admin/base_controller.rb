ccef5461d082b47a904e540d583b1be16005d0f5
shutorm02 <shutorm2106@gmail.com>
2022-09-03 20:51:11 +0900

    Update: flash_message の表示場所を修正

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,8 @@
 class Admin::BaseController < ApplicationController
   before_action :check_admin
   layout 'admin/layouts/application'
+  
+  private
 
   def not_authenticated
     flash[:warning] = t('defaults.message.require_login')
