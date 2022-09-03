f07594c1fdec022c1446df374b8451c7f6699513
shutorm02 <shutorm2106@gmail.com>
2022-09-03 16:49:46 +0900

    Add: ログインしていない場合、admin 用のログイン画面が表示されるようにする

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,13 @@
 class Admin::BaseController < ApplicationController
+  before_action :check_admin
   layout 'admin/layouts/application'
+
+  def not_authenticated
+    flash[:warning] = t('defaults.message.require_login')
+    redirect_to admin_login_path
+  end
+
+  def check_admin
+    redirect_to root_path, warning: t('defaults.message.not_authorized') unless current_user.admin?
+  end
 end
--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,3 +1,7 @@
 class Admin::UserSessionsController < Admin::BaseController
+  skip_before_action :require_login
+  skip_before_action :check_admin
+  layout 'layouts/admin_login'
+
   def new; end
 end
