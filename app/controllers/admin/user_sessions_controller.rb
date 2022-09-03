16536d85cb0dd6ccc74c08678ddb03c011183f5b
shutorm02 <shutorm2106@gmail.com>
2022-09-03 17:22:35 +0900

    Add: admin 画面のログイン、ログアウトを実装

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UserSessionsController < Admin::BaseController
-  skip_before_action :require_login
-  skip_before_action :check_admin
+  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :check_admin, only: %i[new create]
   layout 'layouts/admin_login'
 
   def new; end
@@ -15,7 +15,7 @@ class Admin::UserSessionsController < Admin::BaseController
     end
   end
 
-  def logout
+  def destroy
     logout
     redirect_to admin_login_path, success: t('.success')
   end
