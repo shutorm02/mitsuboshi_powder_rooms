b0a493a98a14782b485929652bd88d041520b3f4
shutorm02 <shutorm2106@gmail.com>
2022-10-15 20:20:53 +0900

    Add: profiles_controller に edit update show を追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,26 @@
 class ProfilesController < ApplicationController
-  def show
+  before_action :set_user, only: %i[edit update]
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, success: t('defaults.message.updated', item: User.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: User.model_name.human)
+      render :edit
+    end
+  end
+
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :avatar, :avatar_cache)
   end
 end
