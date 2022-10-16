b864c905bf7c1f68925416b7ed5952c13c05bd83
shutorm02 <shutorm2106@gmail.com>
2022-10-16 18:56:13 +0900

    Add: User モデルに mount_uploader のコードを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  mount_uploader :avatar, AvatarUploader
 
   has_many :spots, dependent: :destroy
   has_many :likes, dependent: :destroy
