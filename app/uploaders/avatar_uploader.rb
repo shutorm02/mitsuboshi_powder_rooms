f2682b4b14a2540597acb7fdd5a5551732dcce1a
shutorm02 <shutorm2106@gmail.com>
2022-10-16 18:52:59 +0900

    Add: bundle exec rails g uploader Avatar を実行、アバターの設定を追加

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,15 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url
+    'sample.png'
+  end
+
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
+end
