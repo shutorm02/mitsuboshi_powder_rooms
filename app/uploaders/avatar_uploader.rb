92295767eb832d59351cdef3a5a5fb76d19ec407
shutorm02 <shutorm2106@gmail.com>
2022-10-17 16:50:29 +0900

    Update: エラー発生のため、Carrierwaveの設定を修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,9 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-  storage :fog
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
