57288e070e511af256b5d6dc7590e9b943fbe771
shutorm02 <shutorm2106@gmail.com>
2022-10-18 19:31:21 +0900

    Update: config/routes のタイピングミスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  post 'oauths/callback', to: 'oauths#callback'
-  get 'oauths/callback',  to: 'oauths#callback'
-  get 'oauths/:provider', to: 'oauths#oauth', as: :auth_at_provider
+  post 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/callback',  to: 'oauths#callback'
+  get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
 
   resources :users, only: %i[new create destroy]
   resources :spots do
