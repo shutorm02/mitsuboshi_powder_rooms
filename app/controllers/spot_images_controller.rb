f9400d00a22cc308313684cc6638e3f7b8709682
shutorm02 <shutorm2106@gmail.com>
2022-10-20 11:46:39 +0900

    Add: spot_images コントローラーに new create destroy を追加

--- app/controllers/spot_images_controller.rb
+++ app/controllers/spot_images_controller.rb
@@ -1,4 +1,36 @@
 class SpotImagesController < ApplicationController
+  before_action :require_login
+  before_action :set_spot
+
   def new
+    @image = @spot.image.new
+  end
+
+  def edit; end
+
+  def create
+    @image = @spot.image.new(image_params)
+
+    if @image.save
+      redirect_to spot_path(@spot), success: t('.success')
+    else
+      flash.now['danger'] = t('.fail', item: SpotImage.model_name.human)
+      render :new
+  end
+
+  def destroy
+    @image = @spot.image.find(params[:id])
+    @image.destroy!
+    redirect_to spot_path(@spot), success: t('.success')
+  end
+
+  private
+
+  def set_spot
+    @spot = current_user.spots.find(params[:spot_id])
+  end
+  
+  def image_params
+    params.require(:spot_image).permit(:spot_image, :spot_image_cache)
   end
 end
