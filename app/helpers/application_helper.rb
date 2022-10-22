3b13b2cfd02bf8e2616725feee5a03195124ee59
shutorm02 <shutorm2106@gmail.com>
2022-10-22 23:44:14 +0900

    Add: application_helper に meta タグの設定を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,56 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: '三つ星トイレ'
+      title: '日本各地のキレイで設備豊富なトイレが探せる、トイレ検索サービス',
+      description: '「三つ星トイレ」は、たとえ家の外であっても、 三つ星ホテルのようにキレイなトイレを使いたい…、そんな人のためのトイレ検索サービスです。現在地やお近くのスポット、設備などから、あなたのお気に入りのトイレを見つけよう！',
+      keywords: '三つ星トイレ, キレイなトイレ',
+      charset: 'UTF-8',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: [
+        { href: image_pack_tag 'media/images/favicon.png', sizes: '32x32' },
+        { href: image_pack_tag 'media/images/icon.svg', rel: 'apple-touch-icon', sizes: '180x180', type: 'image/svg' },
+      ],
+      og: {
+        site_name: '三つ星トイレ'
+        title: '日本各地のキレイで設備豊富なトイレが探せる検索サービス',
+        description: '「三つ星トイレ」は、たとえ家の外であっても、 三つ星ホテルのようにキレイなトイレを使いたい…、そんな人のためのトイレ検索サービスです。現在地やお近くのスポット、設備などから、あなたのお気に入りのトイレを見つけましょう。',
+        type: 'website',
+        url: request.original_url,
+        image: image_pack_tag 'media/images/ogp.svg',
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@mitsuboshi_PR',
+      }
+    }
+  end
+end
+    {
+      reverse: true,
+      separator: '|',
+      description: '三つ星ホテルのようにキレイで設備豊富なトイレを探せるWebサービス。たとえ家の外であっても、 トイレには妥協したくない！ そんなあなたにおすすめ',
+      keywords: '三つ星トイレ',
+      icon: [
+      ],
+      og: {
+        site: '三つ星トイレ'
+        title: '三つ星トイレ',
+        description: 'たとえ家の外であっても、 三つ星ホテルのようにキレイなトイレを使いたい、 そんな人のためのトイレ検索サービスです',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@ツイッターのアカウント名',
+      }
+      fb: {
+        app_id: '¥facebookのID'
+      }
+    }
+  end
 end
