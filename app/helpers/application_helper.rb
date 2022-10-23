db10babb8ae99df9ddb4d7af93527dc2deaa0621
shutorm02 <shutorm2106@gmail.com>
2022-10-23 10:47:19 +0900

    Update: favicon ほかの位置を assets 内に移動、コードを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,15 +8,15 @@ module ApplicationHelper
       canonical: request.original_url,
       noindex: ! Rails.env.production?,
       icon: [
-        { href: image_pack_tag 'media/images/favicon.png', sizes: '32x32' },
-        { href: image_pack_tag 'media/images/icon.svg', rel: 'apple-touch-icon', sizes: '180x180', type: 'image/svg' },
+        { href: image_url('favicon.png'), sizes: '32x32' },
+        { href: image_url('icon.svg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/svg' },
       ],
       og: {
-        title: '三つ星トイレ - 日本各地のキレイで設備豊富なトイレが探せる、トイレ検索サービス',
+        title: '三つ星トイレ - 日本各地のキレイなトイレが探せるトイレ検索サービス',
         description: '「三つ星トイレ」は、たとえ家の外であっても、 三つ星ホテルのようにキレイなトイレを使いたい…、そんな人のためのトイレ検索サービスです。現在地やお近くのスポット、設備などから、あなたのお気に入りのトイレを見つけましょう。',
         type: 'website',
         url: request.original_url,
-        image: image_pack_tag 'media/images/ogp.svg',
+        image: image_url('ogp.svg'),
         locale: 'ja_JP',
       },
       twitter: {
