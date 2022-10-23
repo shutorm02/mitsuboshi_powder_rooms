3eb3c792c1bfe5725c50f1caf5ffebbe90598cd1
shutorm02 <shutorm2106@gmail.com>
2022-10-23 11:31:21 +0900

    Update: rubocop -a を実行、コードを自動修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,14 @@ module ApplicationHelper
       keywords: '三つ星トイレ, キレイなトイレ',
       charset: 'UTF-8',
       canonical: request.original_url,
-      noindex: ! Rails.env.production?,
+      noindex: !Rails.env.production?,
       icon: [
         { href: image_url('favicon.png'), sizes: '32x32' },
         { href: image_url('icon.svg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/svg' },
       ],
       og: {
         title: '三つ星トイレ - 日本各地のキレイなトイレが探せるトイレ検索サービス',
-      description: '「三つ星トイレ」は、家の外でもトイレ選びに妥協したくない人のためのトイレ検索サービスです。現在地やお近くのスポット・こだわり設備などから、あなたのお気に入りのトイレを見つけよう！',
+        description: '「三つ星トイレ」は、家の外でもトイレ選びに妥協したくない人のためのトイレ検索サービスです。現在地やお近くのスポット・こだわり設備などから、あなたのお気に入りのトイレを見つけよう！',
         type: 'website',
         url: request.original_url,
         image: image_url('ogp.svg'),
@@ -22,7 +22,7 @@ module ApplicationHelper
       twitter: {
         card: 'summary_large_image',
         site: '@mitsuboshi_PR',
-      }
+      },
     }
   end
 end
