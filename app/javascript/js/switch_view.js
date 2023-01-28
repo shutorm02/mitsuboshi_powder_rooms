5149fa4d4294fcc591cd7b1d458eebe69a4f1244
shutorm02 <shutorm2106@gmail.com>
2023-01-28 18:18:05 +0900

    Update: switch_view を hash での変更にする

--- app/javascript/js/switch_view.js
+++ app/javascript/js/switch_view.js
@@ -1,20 +1,26 @@
-window.onload = function () {
-  const btnMap = document.getElementById('btnMap')
-  const btnList = document.getElementById('btnList')
-  const spotsMap = document.getElementById('spotsMap')
-  const spotsList = document.getElementById('spotsList')
+const btnMap = document.querySelector('#btnMap')
+const btnList = document.querySelector('#btnList')
+const spotsMap = document.querySelector('#spotsMap')
+const spotsList = document.querySelector('#spotsList')
 
-  btnMap.onclick = function () {
-    btnMap.classList.toggle('hidden')
-    btnList.classList.toggle('hidden')
-    spotsMap.classList.toggle('hidden')
-    spotsList.classList.toggle('hidden')
-  }
-
-  btnList.onclick = function () {
-    btnMap.classList.toggle('hidden')
-    btnList.classList.toggle('hidden')
-    spotsMap.classList.toggle('hidden')
-    spotsList.classList.toggle('hidden')
-  }
+const switchMapView = function() {
+  btnMap.classList.add('hidden')
+  spotsMap.classList.remove('hidden')
+  btnList.classList.remove('hidden')
+  spotsList.classList.add('hidden')
 }
+
+const switchListView = function() {
+  btnMap.classList.remove('hidden')
+  spotsMap.classList.add('hidden')
+  btnList.classList.add('hidden')
+  spotsList.classList.remove('hidden')
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+  (location.hash == '' || location.hash == "#map") ? switchMapView() : switchListView();
+})
+
+window.addEventListener('hashchange', function() {
+  (location.hash == '' || location.hash == "#map") ? switchMapView() : switchListView();
+})
