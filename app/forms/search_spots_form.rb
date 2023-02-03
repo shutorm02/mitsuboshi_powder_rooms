631fd7f55a7189dbcd927e3dc5d5b43aeedf55eb
shutorm02 <shutorm2106@gmail.com>
2023-02-03 16:05:09 +0900

    Add: 検索フォームから設備詳細の絞り込みができるようにする

--- app/forms/search_spots_form.rb
+++ app/forms/search_spots_form.rb
@@ -2,10 +2,19 @@ class SearchSpotsForm
   include ActiveModel::Model
   include ActiveModel::Attributes
 
+  attribute :created_at
   attribute :equipment_detail_ids
 
   def search
     relation = Spot.includes(:equipment_details).distinct
+
+    if equipment_detail_ids.present?
+      relation = relation.where(equipment_details: {id: equipment_detail_ids}) if equipment_detail_ids.present?
+      spot_ids = relation.ids
+      relation = Spot.includes(:equipment_details).where(id: spot_ids)
+    end
+
+    relation
   end
 
 end
