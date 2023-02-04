30ca6ef78adbb7fc9081d7ddfc51561128812df4
shutorm02 <shutorm2106@gmail.com>
2023-02-04 14:36:44 +0900

    Add: リスト表示時、Map用の検索フォームが表示されないようにする

--- app/javascript/js/switch_view.js
+++ app/javascript/js/switch_view.js
@@ -3,11 +3,15 @@ const btnList = document.querySelector('#btnList')
 const spotsMap = document.querySelector('#spotsMap')
 const spotsList = document.querySelector('#spotsList')
 
+const mapSearchForm = document.querySelector('#mapSearchForm')
+
 const switchMapView = function() {
   btnMap.classList.add('hidden')
   spotsMap.classList.remove('hidden')
   btnList.classList.remove('hidden')
   spotsList.classList.add('hidden')
+  btnList.classList.remove('hidden')
+  mapSearchForm.classList.remove('hidden')
 }
 
 const switchListView = function() {
@@ -15,6 +19,7 @@ const switchListView = function() {
   spotsMap.classList.add('hidden')
   btnList.classList.add('hidden')
   spotsList.classList.remove('hidden')
+  mapSearchForm.classList.add('hidden')
 }
 
 window.addEventListener('DOMContentLoaded', function() {
