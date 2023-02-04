e242f4defb022faa1242d985e5d956ead43e3e5f
shutorm02 <shutorm2106@gmail.com>
2023-02-04 15:08:27 +0900

    Add: 検索フォームをアコーディオンにする

--- app/javascript/js/search_form.js
+++ app/javascript/js/search_form.js
@@ -1,1 +1,10 @@
-console.log('アコーディオンを作るぞ')
+$('.accordion-btn').on('click', function() {
+  let body = $(this).next('.accordion-body');
+  $(body).slideToggle();
+
+  if($(body).hasClass('hidden')) {
+    $(body).removeClass('hidden');
+  } else {
+    $(body).addClass('hidden');
+  }
+});
